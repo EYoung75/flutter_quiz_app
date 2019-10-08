4f053449ccbeed0247da8a21060f1bf0834d2e04
EYoung75 <eyoung75.ey@gmail.com>
2019-10-08 16:24:20 -0400

    finished simple quiz app to solidify concepts of passing data, app hiercarchy and splitting code into individual widgets

--- lib/main.dart
+++ lib/main.dart
@@ -45,7 +45,7 @@ class _MyAppState extends State<MyApp> {
   var _questionIndex = 0;
   var _totalScore = 0;
 
-  void _resetQuiz() {
+  void _resetQuiz() { 
     setState(() {
       _questionIndex = 0;
       _totalScore = 0;
