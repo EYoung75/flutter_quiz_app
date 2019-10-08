bb97bd994d57dc3c875bd772454a5fe0c738db44
EYoung75 <eyoung75.ey@gmail.com>
2019-10-08 16:18:48 -0400

    created reset function and button to reset quiz after finished

--- lib/result.dart
+++ lib/result.dart
@@ -2,16 +2,17 @@ import "package:flutter/material.dart";
 
 class Result extends StatelessWidget {
   final int finalScore;
+  final Function resetQuiz;
 
-  Result(this.finalScore);
+  Result(this.finalScore, this.resetQuiz);
 
   String get resultPhrase {
     var resultText = "Congrats";
-    if(finalScore <=7) {
+    if (finalScore <= 7) {
       resultText = "You are the best around";
-    } else if(finalScore <= 14) {
+    } else if (finalScore <= 14) {
       resultText = "Not too shabby";
-    } else if(finalScore <=21) {
+    } else if (finalScore <= 21) {
       resultText = "Not very likeable";
     } else {
       resultText = "You literally suck";
@@ -22,10 +23,18 @@ class Result extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text(
-        resultPhrase,
-        style: TextStyle(fontSize: 36.0, fontWeight: FontWeight.bold),
-        textAlign: TextAlign.center,
+      child: Column(
+        children: <Widget>[
+          Text(
+            resultPhrase,
+            style: TextStyle(fontSize: 36.0, fontWeight: FontWeight.bold),
+            textAlign: TextAlign.center,
+          ),
+          RaisedButton(
+            child: Text("Restart quiz"),
+            onPressed: resetQuiz,
+          )
+        ],
       ),
     );
   }
